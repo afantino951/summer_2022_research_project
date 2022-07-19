62621c6aa44341a23e6863edf6492d6c995b8c62
afantino951 <andrewfa@g.ucla.edu>
2022-07-18 22:44:27 -0700

    Fix pylint warnings for client.py

--- socket_sample/client.py
+++ socket_sample/client.py
@@ -10,17 +10,16 @@ import socket
 HOST = "127.0.0.1" # This is the loop-back interface
 PORT = 65432
 
-def init_connection():
-    pass
+def init_client_connection():
+    """ Init the client connection """
+    return
 
 
 if __name__ == "__main__":
-    
     # Just the sample echo client and server from  https://realpython.com/python-sockets/
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         s.sendall(b"Hello World")
         data = s.recv(1024)
 
     print(f"Received {data!r}")
-    
