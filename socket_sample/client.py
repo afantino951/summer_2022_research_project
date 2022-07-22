47e1e9e5cde8c959b8dc263df83e4329c999d015
afantino951 <andrewfa@g.ucla.edu>
2022-07-21 21:53:07 -0700

    Add research link and np array example

--- socket_sample/client.py
+++ socket_sample/client.py
@@ -5,6 +5,7 @@ Author: Andrew Fantino
 
 # Imports
 import socket
+import numpy as np
 
 # Global Constants
 HOST = "127.0.0.1" # This is the loop-back interface
@@ -16,6 +17,13 @@ def init_client_connection():
 
 
 if __name__ == "__main__":
+    # Create a np array of ones that is 256x256 bytes of '0xA'
+
+    n_arr = np.array([[1, 2, 3], [4, 5, 6]])
+    
+    # See this link for correct compression: 
+    # https://stackoverflow.com/questions/26377023/send-a-multidimensional-numpy-array-over-a-socket  
+ 
     # Just the sample echo client and server from  https://realpython.com/python-sockets/
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
