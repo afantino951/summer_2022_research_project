de6d9ad3abc06afec2b31bde58cc27f3093da62e
Andrew Fantino <stewe951@gmail.com>
2022-08-13 14:22:07 -0700

    A working prototype of socket portion of neurosurgeon

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # summer_2022_research_project
 Raspi Sample Implementation of "Neurosurgeon: Collaborative Intelligence Between Cloud and Mobile Edge"
+
+# Some resources
+- A github repo that alos compression of np-arrays then sends over a socket:  <https://github.com/sabjorn/NumpySocket>
+    - I'll probably add a submodule to this repo in the future, but I rather do other things. :)
+- If the above github repo doesn't work, then check this out: https://pypi.org/project/mlsocket/)
--- socket_sample/client.py
+++ socket_sample/client.py
@@ -4,30 +4,30 @@ Author: Andrew Fantino
 """
 
 # Imports
-import socket
 import numpy as np
+from numpysocket import NumpySocket
+from time import sleep
 
 # Global Constants
 HOST = "127.0.0.1" # This is the loop-back interface
-PORT = 65432
-
-def init_client_connection():
-    """ Init the client connection """
-    return
-
+PORT = 9999
 
 if __name__ == "__main__":
     # Create a np array of ones that is 256x256 bytes of '0xA'
+    n_arr = np.array([[1, 2, 3], [4, 5, 6]]) # <- is not ^ (yet)
 
-    n_arr = np.array([[1, 2, 3], [4, 5, 6]])
+    npSocket = NumpySocket()
+
+    while(True):
+        try: 
+            npSocket.startClient(HOST, PORT)
+            break
+        except:
+            print('Connection failed')
+            sleep(1)
+            continue
+
+    npSocket.send(n_arr)
+    print('sent arr')
     
-    # See this link for correct compression: 
-    # https://stackoverflow.com/questions/26377023/send-a-multidimensional-numpy-array-over-a-socket  
- 
-    # Just the sample echo client and server from  https://realpython.com/python-sockets/
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(b"Hello World")
-        data = s.recv(1024)
-
-    print(f"Received {data!r}")
+    npSocket.close()
